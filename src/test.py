b08f6703a88ce128f33dff19081d7bd26c2aa211
Sebastian <sousaseb14@gmail.com>
2021-06-07 14:13:10 -0600

    Created src folder. Placed  code inside

---
+++ src/test.py
@@ -0,0 +1,31 @@
+import sys
+import json
+print(sys.executable)
+print(sys.version)
+
+for num in [1, 2, 3, 4]:
+    print(num)
+ex_string= '''
+{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+'''
